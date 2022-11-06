83c8e109b3e508e9e57a6b74c44018c6e6343bb7
JefferryEx <1624410543@qq.com>
2022-11-06 06:17:30 +0000

    update

--- jefferrys-blog-front/src/components/NavBar.vue
+++ jefferrys-blog-front/src/components/NavBar.vue
@@ -11,12 +11,12 @@
             </span>
             <a v-on:click="IsShowFunc" class="flex md:hidden items-center mr-4"><svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16" width="16px" height="16px"><path d="M 1 2 L 1 3 L 14 3 L 14 2 Z M 1 7 L 1 8 L 14 8 L 14 7 Z M 1 12 L 1 13 L 14 13 L 14 12 Z"/></svg></a>
         </div>
-        <div :class="{hidden : !isShow}" class="flex absolute z-999 w-full h-76 bg-gray-800">
-            <div id="container" class="grid grid-flow-row m-auto border-x-1 border-black">
-                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-500">Home</a>
-                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-500">Blogs</a>
-                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-500">Introduction</a>
-                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-500">Open Source</a>
+        <div :class="{hidden : !isShow}" class="flex absolute z-50 w-full h-80 bg-gray-800">
+            <div id="container" class="grid grid-flow-row m-auto border-x-1 border-white">
+                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-100 border-x-1 border-white">Home</a>
+                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-100 border-x-1 border-white">Blogs</a>
+                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-100 border-x-1 border-white">Introduction</a>
+                <a class="col-span-2 text-xl mx-4 text-center my-4 font-sans items-center text-gray-100 border-x-1 border-white">Open Source</a>
             </div>
         </div>
         <!-- <img src="https://ts1.cn.mm.bing.net/th/id/R-C.02f9b3494460578c28aff6fe6a637682?rik=fXFWJhjiwM2K2w&riu=http%3a%2f%2fn.sinaimg.cn%2fsinakd20210425ac%2f138%2fw1080h658%2f20210425%2f4e60-kphwumq8147616.jpg&ehk=aBd0ufPukeZ56IsMYvoDIO1dQQBL4DSFeDS8ciKrLVQ%3d&risl=&pid=ImgRaw&r=0" alt=""> -->
--- jefferrys-blog-front/src/views/MainView.vue
+++ jefferrys-blog-front/src/views/MainView.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex mainview w-full h-64 bg-left bg-no-repeat z-0" style="background-image: url('http://rju4cbt6f.hn-bkt.clouddn.com/dddacd405d7e11ed87f40dafd6220406');">
-    <div class="flex items-center absolute right-0" id='ctner ' >
-      <div class="">
-        <p class="font-bold text-2xl text-opacity-75 text-black text-right">Welcome To</p>
-        <p class="font-bold text-2xl text-opacity-75 text-black text-right">Jefferry's Blog</p>
+  <div class="flex w-full h-64 bg-left bg-no-repeat sm:w-96 justify-center" style="background-image: url('http://rju4cbt6f.hn-bkt.clouddn.com/dddacd405d7e11ed87f40dafd6220406');">
+    <div class="flex items-center z-10">
+      <div class="right-2 sm:right-16  absolute z-10">
+        <p class="font-bold text-3xl sm:leading-10 sm:text-5xl text-opacity-75 text-black text-right">Welcome To</p>
+        <p class="font-bold text-2xl sm:leading-14 sm:text-4xl text-opacity-75 text-black text-right">Jefferry's Blog</p>
       </div>
       <!-- <img class="w-full rounded-2xl"
       src="https://ts1.cn.mm.bing.net/th/id/R-C.02f9b3494460578c28aff6fe6a637682?rik=fXFWJhjiwM2K2w&riu=http%3a%2f%2fn.sinaimg.cn%2fsinakd20210425ac%2f138%2fw1080h658%2f20210425%2f4e60-kphwumq8147616.jpg&ehk=aBd0ufPukeZ56IsMYvoDIO1dQQBL4DSFeDS8ciKrLVQ%3d&risl=&pid=ImgRaw&r=0" alt="">
