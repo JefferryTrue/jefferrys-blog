a376846e6696d905a7c2d9efe0dae51ba0e5f619
JefferryEx <1624410543@qq.com>
2022-11-06 11:33:59 +0000

    update

--- jefferrys-blog-front/src/views/MainView.vue
+++ jefferrys-blog-front/src/views/MainView.vue
@@ -1,15 +1,47 @@
 <template>
-  <div class="flex w-full h-64 bg-left bg-no-repeat sm:w-96 justify-center" style="background-image: url('http://rju4cbt6f.hn-bkt.clouddn.com/dddacd405d7e11ed87f40dafd6220406');">
-    <div class="flex items-center z-10">
-      <div class="right-2 sm:right-16  absolute z-10">
-        <p class="font-bold text-3xl sm:leading-10 sm:text-5xl text-opacity-75 text-black text-right">Welcome To</p>
-        <p class="font-bold text-2xl sm:leading-14 sm:text-4xl text-opacity-75 text-black text-right">Jefferry's Blog</p>
-      </div>
-      <!-- <img class="w-full rounded-2xl"
-      src="https://ts1.cn.mm.bing.net/th/id/R-C.02f9b3494460578c28aff6fe6a637682?rik=fXFWJhjiwM2K2w&riu=http%3a%2f%2fn.sinaimg.cn%2fsinakd20210425ac%2f138%2fw1080h658%2f20210425%2f4e60-kphwumq8147616.jpg&ehk=aBd0ufPukeZ56IsMYvoDIO1dQQBL4DSFeDS8ciKrLVQ%3d&risl=&pid=ImgRaw&r=0" alt="">
-      <h1 class="">Jefferry's Blog</h1> -->
-    </div>
+  <div class="w-full h-screen overflow-y-hidden">
+    <div class="md:grid md:grid-cols-8 h-full relative" >
 
+      <div class="h-screen grid-rows-6 hidden md:grid md:col-span-2 md:col-start-1">
+       <span class="flex mx-auto row-start-1 row-end-3 row-span-2 items-center justify-center">
+          <div class="items-center text-center ">
+            <img class="rounded-full  border-neutral-800 border-2 w-32" src="http://rju4cbt6f.hn-bkt.clouddn.com/a04f92305db011ed87f40dafd6220406" alt="">
+            <p class="mt-2 font-bold text-lg">@Jefferry True</p>
+            <p class="mt-2 font-thin text-base">ChongQing-China</p>
+          </div>
+        </span> 
+        <!--Un Do-->
+       <span class="grid grid-rows-6 row-start-3 row-end-5 row-span-3 font-semibold text-xl text-gray-700 w-full">
+        <div       class=" row-span-1  w-full"></div>
+        <a href="" class="row-span-1"><span class="m-auto">123</span></a>
+        
+       </span>
+       <span class="row-span-1 row-start-5 row-end-6">123</span>
+      </div>
+      <!--Un Do-->
+      <div class="w-full h-auto md:col-span-6 md:col-start-3">
+        <div class="flex w-full h-64 lg:h-80 bg-left-top bg-no-repeat  xl:hidden" style="background-image: url('http://rju4cbt6f.hn-bkt.clouddn.com/dddacd405d7e11ed87f40dafd6220406');">
+          <div class="flex items-center z-10">
+            <div class="right-2 sm:right-16 md:right-18 lg:right-18 xl:right-26 2xl:right-32 absolute z-10">
+              <p class="font-bold text-3xl sm:leading-10 sm:text-5xl md:text-5xl lg:text-6xl text-opacity-75 text-black text-right">Welcome To</p>
+              <p class="font-bold text-2xl sm:leading-14 sm:text-4xl md:text-4xl lg:text-5xl text-opacity-75 text-black text-right">Jefferry's Blog</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="w-full h-80 bg-left bg-no-repeat hidden xl:flex bg-cover" style="background-image: url('http://rju4cbt6f.hn-bkt.clouddn.com/ecafc2205da211ed87f40dafd6220406');">
+          <div class="flex items-center z-10">
+            <div class="right-2 sm:right-16 md:right-18 lg:right-18 xl:right-26 2xl:right-32 absolute z-10">
+              <p class="font-bold text-3xl sm:leading-10 sm:text-5xl md:text-5xl lg:text-6xl text-opacity-75 text-black text-right">Welcome To</p>
+              <p class="font-bold text-2xl sm:leading-14 sm:text-4xl md:text-4xl lg:text-5xl text-opacity-75 text-black text-right">Jefferry's Blog</p>
+            </div>
+          </div>
+  
+        </div>
+        123
+      </div>
+
+    </div>
   </div>
 </template>
 
